570278e61ae6db6e1fbbb9d8c1e45e503037027e
asphidel <asphidel@gmail.com>
2014-06-10 18:15:31 -0500

    Main.lua w/ empty callback functions

---
+++ main.lua
@@ -0,0 +1,20 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.keypressed(key)
+end
+
+function love.keyreleased(key)
+end
+
+function love.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+end
+
+function love.draw()
+end
